a5fdfc905e68acadf3283052a971792c9bbd42bb
junwvwv <wvwv0612@gmail.com>
2023-11-19 10:17:15 +0900

    상품 등록, 수정, 조회

---
+++ shop_v1/src/main/java/com/shop/controller/BookForm.java
@@ -0,0 +1,20 @@
+package com.shop.controller;
+
+import lombok.Data;
+
+@Data
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    private String author;
+
+    private String isbn;
+
+}
